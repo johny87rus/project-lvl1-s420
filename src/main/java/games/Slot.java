3b9cb63f4e158795185747b7c1ee43414880cfcc
emikhaylov <emikhaylov@diasoft.ru>
2018-09-26 00:25:36 +0500

    add test code for plugins (pmd, spotbugs)

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -2,6 +2,14 @@ package games;
 
 public class Slot {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!" + " TEST   !!!");
     }
+
+//    private static final String RULESET = "category/java/errorprone.xml"; // для проверки плагина PMD
+//
+//    class BadCase {    // класс для проверки плагина spotbugs
+//        void method() {
+//            System.out.println("Hello SpotBugs!");
+//        }
+//    }
 }
