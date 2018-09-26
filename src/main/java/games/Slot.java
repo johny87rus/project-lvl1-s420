a2b31e9b3b8ee7b477f81282598600958f9aa9b9
emikhaylov <emikhaylov@diasoft.ru>
2018-09-26 18:59:15 +0500

    step 4

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -1,15 +1,39 @@
 package games;
 
+import static java.lang.Math.random;
+import static java.lang.Math.round;
+
 public class Slot {
     public static void main(String[] args) {
-        System.out.println("Hello, World!" + " TEST   !!!");
+        final int DEFAULT_CASH = 100;
+        final int DEFAULT_BET = 10;
+        final int DEFAULT_WIN = 10000;
+        final int SIZE = 7;
+        final int START_COUNTER = 0;
+
+        int cash = DEFAULT_CASH;
+        int bet = DEFAULT_BET;
+        int win = DEFAULT_WIN;
+        int size = SIZE;
+        int firstCounter, secondCounter, thirdCounter;
+        firstCounter = secondCounter = thirdCounter = START_COUNTER;
+
+        while(cash>0) {
+            System.out.printf("У Вас %d$, ставка - %d$%n", cash, bet);
+            System.out.println("Крутим барабаны!Розыгрыш принёс следующие результаты:");
+            firstCounter = (firstCounter + (int) round(random() * 100)) % size;
+            secondCounter = (secondCounter + (int) round(random() * 100)) % size;
+            thirdCounter = (thirdCounter + (int) round(random() * 100)) % size;
+            System.out.printf("первый барабан - %d, второй - %d, третий - %d%n", firstCounter, secondCounter, thirdCounter);
+            if (firstCounter==secondCounter && secondCounter==thirdCounter) {
+                cash += win;
+                System.out.printf("Поздравляем! Вы выиграли %d$ ! Ваш капитал теперь составляет: %d$%n", win, cash);
+            } else {
+                cash -= bet;
+                System.out.printf("Проигрыш %d$, ваш капитал теперь составляет: %d$%n", bet, cash);
+            }
+        }
     }
 
-//    private static final String RULESET = "category/java/errorprone.xml"; // для проверки плагина PMD
-//
-//    class BadCase {    // класс для проверки плагина spotbugs
-//        void method() {
-//            System.out.println("Hello SpotBugs!");
-//        }
-//    }
+
 }
