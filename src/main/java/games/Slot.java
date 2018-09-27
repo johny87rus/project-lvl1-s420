20ea67bf1ca7d368e651877051e96e46b18d2c15
emikhaylov <emikhaylov@diasoft.ru>
2018-09-27 19:35:43 +0500

    * Увеличил DEFAULT_SIZE до 8, чтобы выпадали семерочки на барабане.
    * Переименовал дефолтные константы в один стиль
    
    * Выполнил рекомендации SonarLint :
      - заменил Math.Random и преобразования в int -> Random.nextInt(size);
      - разделил групповое объявление переменных на раздельное;

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -1,29 +1,31 @@
 package games;
 
-import static java.lang.Math.random;
-import static java.lang.Math.round;
+import java.util.Random;
 
 public class Slot {
     public static void main(String[] args) {
         final int DEFAULT_CASH = 100;
         final int DEFAULT_BET = 10;
         final int DEFAULT_WIN = 10000;
-        final int SIZE = 7;
-        final int START_COUNTER = 0;
+        final int DEFAULT_SIZE = 8;
+        final int DEFAULT_START_COUNTER = 0;
 
         int cash = DEFAULT_CASH;
         int bet = DEFAULT_BET;
         int win = DEFAULT_WIN;
-        int size = SIZE;
-        int firstCounter, secondCounter, thirdCounter;
-        firstCounter = secondCounter = thirdCounter = START_COUNTER;
+        int size = DEFAULT_SIZE;
+        int firstCounter;
+        int secondCounter;
+        int thirdCounter;
+        firstCounter = secondCounter = thirdCounter = DEFAULT_START_COUNTER;
+        Random random = new Random();
 
         while(cash>0) {
             System.out.printf("У Вас %d$, ставка - %d$%n", cash, bet);
             System.out.println("Крутим барабаны!Розыгрыш принёс следующие результаты:");
-            firstCounter = (firstCounter + (int) round(random() * 100)) % size;
-            secondCounter = (secondCounter + (int) round(random() * 100)) % size;
-            thirdCounter = (thirdCounter + (int) round(random() * 100)) % size;
+            firstCounter = (firstCounter + random.nextInt(size)) % size;
+            secondCounter = (secondCounter + random.nextInt(size)) % size;
+            thirdCounter = (thirdCounter + random.nextInt(size)) % size;
             System.out.printf("первый барабан - %d, второй - %d, третий - %d%n", firstCounter, secondCounter, thirdCounter);
             if (firstCounter==secondCounter && secondCounter==thirdCounter) {
                 cash += win;
