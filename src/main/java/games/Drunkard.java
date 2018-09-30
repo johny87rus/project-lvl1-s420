d9c856c3b13cd0e0451d411d9c6e4f9b4427dc4a
emikhaylov <emikhaylov@diasoft.ru>
2018-09-30 14:05:03 +0500

    Пьяница refactor - use CardUtils

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -2,9 +2,10 @@ package games;
 
 import org.apache.commons.math3.util.MathArrays;
 
+import static games.CardUtils.CARDS_TOTAL_COUNT;
+import static games.CardUtils.getPar;
+
 public class Drunkard {
-    private static final int PARS_TOTAL_COUNT = Par.values().length;
-    private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length;
     private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT];
     private static int[] playersCardsBeginCursors = new int[2];
     private static int[] playersCardsEndCursors = new int[2];
@@ -27,15 +28,15 @@ public class Drunkard {
             playersCardsBeginCursors[0]=incrementIndex(playersCardsBeginCursors[0]);
             player2Card = player2Cards[playersCardsBeginCursors[1]];
             playersCardsBeginCursors[1]=incrementIndex(playersCardsBeginCursors[1]);
-            System.out.printf("Игрок №1 карта : %s, игрок №2 карта : %s. ", toString(player1Card), toString(player2Card));
-            if (getPar(player1Card).ordinal()>getPar(player2Card).ordinal() && !(getPar(player2Card)==Par.SIX && getPar(player1Card)==Par.ACE) || getPar(player1Card)==Par.SIX && getPar(player2Card)==Par.ACE) {
+            System.out.printf("Игрок №1 карта : %s, игрок №2 карта : %s. ", CardUtils.toString(player1Card), CardUtils.toString(player2Card));
+            if (getPar(player1Card).ordinal()>getPar(player2Card).ordinal() && !(getPar(player2Card)== CardUtils.Par.SIX && getPar(player1Card)== CardUtils.Par.ACE) || getPar(player1Card)== CardUtils.Par.SIX && getPar(player2Card)== CardUtils.Par.ACE) {
                 player1Cards[playersCardsEndCursors[0]]=player1Card;
                 playersCardsEndCursors[0]=incrementIndex(playersCardsEndCursors[0]);
                 player1Cards[playersCardsEndCursors[0]]=player2Card;
                 playersCardsEndCursors[0]=incrementIndex(playersCardsEndCursors[0]);
                 System.out.print("Выиграл игрок 1!");
                 flag=1;
-            } else if (getPar(player1Card).ordinal()<getPar(player2Card).ordinal() || getPar(player2Card)==Par.SIX && getPar(player1Card)==Par.ACE) {
+            } else if (getPar(player1Card).ordinal()<getPar(player2Card).ordinal() || getPar(player2Card)== CardUtils.Par.SIX && getPar(player1Card)== CardUtils.Par.ACE) {
                 player2Cards[playersCardsEndCursors[1]]=player2Card;
                 playersCardsEndCursors[1]=incrementIndex(playersCardsEndCursors[1]);
                 player2Cards[playersCardsEndCursors[1]]=player1Card;
@@ -59,37 +60,6 @@ public class Drunkard {
             System.out.printf(" У игрока №1 %d карт, у игрока №2 %d карт%n", sum1, sum2);
         }
         System.out.println("Игра окончена!");
-    }
-
-    enum Suit {
-        SPADES, // пики
-        HEARTS, // червы
-        CLUBS, // трефы
-        DIAMONDS // бубны
-    }
-
-    enum Par {
-        SIX,
-        SEVEN,
-        EIGHT,
-        NINE,
-        TEN,
-        JACK, // Валет
-        QUEEN, // Дама
-        KING, // Король
-        ACE // Туз
-    }
-
-    private static Par getPar(int cardNumber) {
-        return Par.values()[cardNumber % PARS_TOTAL_COUNT];
-    }
-
-    private static Suit getSuite(int cardNumber) {
-        return Suit.values()[cardNumber / PARS_TOTAL_COUNT];
-    }
-
-    private static String toString(int cardNumber) {
-        return getPar(cardNumber) + " " + getSuite(cardNumber);
     }
 
     private static int incrementIndex(int i) {
