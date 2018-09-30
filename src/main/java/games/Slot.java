7b7be3f10bddf359c5d6c38c75285381239591a2
emikhaylov <emikhaylov@diasoft.ru>
2018-09-30 12:08:19 +0500

    Code-review fix (Slot)

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -3,36 +3,34 @@ package games;
 import java.util.Random;
 
 public class Slot {
-    public static final int DEFAULT_CASH = 100;
-    public static final int DEFAULT_BET = 10;
-    public static final int DEFAULT_WIN = 10000;
-    public static final int DEFAULT_SIZE = 8;
-    public static final int DEFAULT_START_COUNTER = 0;
+    private static final int CASH = 100;
+    private static final int BET = 10;
+    private static final int WIN = 10000;
+    private static final int SIZE = 8;
+    private static final int START_COUNTER = 0;
 
     public static void main(String[] args) {
-        int cash = DEFAULT_CASH;
-        int bet = DEFAULT_BET;
-        int win = DEFAULT_WIN;
-        int size = DEFAULT_SIZE;
+
         int firstCounter;
         int secondCounter;
         int thirdCounter;
-        firstCounter = secondCounter = thirdCounter = DEFAULT_START_COUNTER;
+        firstCounter = secondCounter = thirdCounter = START_COUNTER;
         Random random = new Random();
+        int cash = CASH;
 
         while(cash>0) {
-            System.out.printf("У Вас %d$, ставка - %d$%n", cash, bet);
+            System.out.printf("У Вас %d$, ставка - %d$%n", CASH, BET);
             System.out.println("Крутим барабаны!Розыгрыш принёс следующие результаты:");
-            firstCounter = (firstCounter + random.nextInt(size)) % size;
-            secondCounter = (secondCounter + random.nextInt(size)) % size;
-            thirdCounter = (thirdCounter + random.nextInt(size)) % size;
+            firstCounter = (firstCounter + random.nextInt(SIZE)) % SIZE;
+            secondCounter = (secondCounter + random.nextInt(SIZE)) % SIZE;
+            thirdCounter = (thirdCounter + random.nextInt(SIZE)) % SIZE;
             System.out.printf("первый барабан - %d, второй - %d, третий - %d%n", firstCounter, secondCounter, thirdCounter);
             if (firstCounter==secondCounter && secondCounter==thirdCounter) {
-                cash += win;
-                System.out.printf("Поздравляем! Вы выиграли %d$ ! Ваш капитал теперь составляет: %d$%n", win, cash);
+                cash += WIN;
+                System.out.printf("Поздравляем! Вы выиграли %d$ ! Ваш капитал теперь составляет: %d$%n", WIN, cash);
             } else {
-                cash -= bet;
-                System.out.printf("Проигрыш %d$, ваш капитал теперь составляет: %d$%n", bet, cash);
+                cash -= BET;
+                System.out.printf("Проигрыш %d$, ваш капитал теперь составляет: %d$%n", BET, cash);
             }
         }
     }
