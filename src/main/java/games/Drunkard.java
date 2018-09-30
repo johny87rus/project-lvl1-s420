d68d9c16178f5feafb0c5551fb2f252f9b35a233
emikhaylov <emikhaylov@diasoft.ru>
2018-09-30 13:20:04 +0500

    Code-review fix : - исправлен условие завершения вместо заглушки - добавлено правило игры - "Шестерку бьёт не всё" - исправлена опечатка, приводившая к задвоению карт у первого игрока в случае победы второго игрока.

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -2,6 +2,8 @@ package games;
 
 import org.apache.commons.math3.util.MathArrays;
 
+import java.util.Arrays;
+
 
 public class Drunkard {
     private static final int PARS_TOTAL_COUNT = Par.values().length;
@@ -10,37 +12,36 @@ public class Drunkard {
     private static int[] playersCardsBeginCursors = new int[2];
     private static int[] playersCardsEndCursors = new int[2];
 
-
     public static void main(String[] args) {
         generateCards(playersCards);
         int[] player1Cards = playersCards[0];
         int[] player2Cards = playersCards[1];
         int player1Card;
         int player2Card;
-        int sum1;
-        int sum2;
+        int sum1=CARDS_TOTAL_COUNT/2;
+        int sum2=CARDS_TOTAL_COUNT/2;
         int flag;
         playersCardsBeginCursors[0]=0;
         playersCardsBeginCursors[1]=0;
         playersCardsEndCursors[0]=CARDS_TOTAL_COUNT/2;
         playersCardsEndCursors[1]=CARDS_TOTAL_COUNT/2;
-        while(!gameOver()) {
+        while(sum1!=CARDS_TOTAL_COUNT && sum2!=CARDS_TOTAL_COUNT) {
             player1Card = player1Cards[playersCardsBeginCursors[0]];
             playersCardsBeginCursors[0]=incrementIndex(playersCardsBeginCursors[0]);
             player2Card = player2Cards[playersCardsBeginCursors[1]];
             playersCardsBeginCursors[1]=incrementIndex(playersCardsBeginCursors[1]);
             System.out.printf("Игрок №1 карта : %s, игрок №2 карта : %s. ", toString(player1Card), toString(player2Card));
-            if (getPar(player1Card).ordinal()>getPar(player2Card).ordinal()) {
+            if (getPar(player1Card).ordinal()>getPar(player2Card).ordinal() || (getPar(player1Card)==Par.SIX && getPar(player2Card)==Par.ACE)) {
                 player1Cards[playersCardsEndCursors[0]]=player1Card;
                 playersCardsEndCursors[0]=incrementIndex(playersCardsEndCursors[0]);
                 player1Cards[playersCardsEndCursors[0]]=player2Card;
                 playersCardsEndCursors[0]=incrementIndex(playersCardsEndCursors[0]);
                 System.out.print("Выиграл игрок 1!");
                 flag=1;
-            } else if (getPar(player1Card).ordinal()<getPar(player2Card).ordinal()) {
+            } else if (getPar(player1Card).ordinal()<getPar(player2Card).ordinal() || (getPar(player2Card)==Par.SIX && getPar(player1Card)==Par.ACE)) {
                 player2Cards[playersCardsEndCursors[1]]=player2Card;
                 playersCardsEndCursors[1]=incrementIndex(playersCardsEndCursors[1]);
-                player1Cards[playersCardsEndCursors[1]]=player1Card;
+                player2Cards[playersCardsEndCursors[1]]=player1Card;
                 playersCardsEndCursors[1]=incrementIndex(playersCardsEndCursors[1]);
                 System.out.print("Выиграл игрок 2!");
                 flag=2;
@@ -59,7 +60,6 @@ public class Drunkard {
                 sum2 = flag==2 ? sum2 : 0;
             }
             System.out.printf(" У игрока №1 %d карт, у игрока №2 %d карт%n", sum1, sum2);
-            if (sum1==CARDS_TOTAL_COUNT || sum2==CARDS_TOTAL_COUNT) break;
         }
         System.out.println("Игра окончена!");
     }
@@ -111,10 +111,4 @@ public class Drunkard {
         }
 
     }
-
-    private static boolean gameOver () {
-        return false;
-    }
-
-
 }
